8d3473b0d4cb24a8ba611e51d84ba9650665ba5e
gabbyhontz <143756143+gabbyhontz@users.noreply.github.com>
2023-09-13 10:39:30 -0400

    functions and initializing variables

--- Pro5.cpp
+++ Pro5.cpp
@@ -5,6 +5,29 @@
 
 using namespace std;
 
+//initializing the variables used in the code
+double z=0;
+double sigma_z = 0, grad_sig_z=0;
+
+//function where the variable z in the given equation is inputted and returned
+double input_z() {
+	cout << "Type in a value for z (with at least one decimal place): ";
+	cin >> z;
+	return z;
+}
+
+//function sigmoid where the equation for sigma(z) is alculated using the inputted z value and returned
+double sigmoid() {
+	sigma_z = 1 / (1 + exp(-z));
+	return sigma_z;
+}
+
+//function grad_sigmoid where the equation for the gradient of sigma(z) is stored and calculated using the returned sigma_z value
+double gradient_sigmoid() {
+	grad_sig_z = sigma_z * (1 - (sigma_z));
+	return grad_sig_z;
+}
+
 int main()
 {
 	
