d3c8543d03224eec2d0b2e799ed2ac7674e4ccc0
gabbyhontz <143756143+gabbyhontz@users.noreply.github.com>
2023-09-21 11:04:38 -0400

    working code

--- Pro4.cpp
+++ Pro4.cpp
@@ -5,27 +5,50 @@
 
 using namespace std;
 
-int inputA(int vect_A)
-{
-    int size;
+double dot_product() {
+    double dot_pro=0; //initialize dot_pro
 
+    int size; //initializing size vector
+    cout << "What is the size of your vectors A and B?" << endl;
+    cin >> size; //taking size of vector from user
 
-    cout << "Input the size of both matrices? \n" << endl;
-    cin >> size;
+    cout << endl;
 
+    double var_a; //initialize input of vector components
+    vector<double> vect_a;
     for (double i = 0; i < size; i++)
+    {
+        cout << "what is the value for element " << i << " in your vector A? " << endl;
+        cin >> var_a;
+        vect_a.push_back(var_a);
+    }
+    
+    cout << endl;
 
-        vect_A = vect_A[i];
+    double var_b;
+    vector<double> vect_b;
+    for (double i = 0; i < size; i++)
+    {
+        cout << "what is the value for element " << i << " in your vector B? " << endl;
+        cin >> var_b;
+        vect_b.push_back(var_b);
+    }
+    
+    cout << endl;
 
-    return vect_A;
+    cout << "The answer is: " << endl;
+    for (double i = 0; i < size; i++) {
+        //its only multiplying the last two entries in each input (0.0001*20.945)
+        dot_pro = dot_pro+vect_a[i] * vect_b[i];
+    }
+    
+
+    return dot_pro;
 }
 
 int main()
 {
-    int product = 0;
-
-
-
-    product = vect_A * vect_B;
+   cout << dot_product() << endl;
+	
 	return 0;
 }
