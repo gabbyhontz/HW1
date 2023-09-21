ce2fe4da13fbb0e4e47051941ea502afdf17d9cc
gabbyhontz <143756143+gabbyhontz@users.noreply.github.com>
2023-09-21 12:18:08 -0400

    comment (final)

--- Pro4.cpp
+++ Pro4.cpp
@@ -15,39 +15,43 @@ double dot_product() {
     cout << endl;
 
     double var_a; //initialize input of vector components
-    vector<double> vect_a;
+    vector<double> vect_a; //initializing vector with variable values as double
     for (double i = 0; i < size; i++)
     {
         cout << "what is the value for element " << i << " in your vector A? " << endl;
-        cin >> var_a;
-        vect_a.push_back(var_a);
+        cin >> var_a; //input for vector a variables
+        vect_a.push_back(var_a); //used to push elements into a vector from the back
     }
     
     cout << endl;
 
-    double var_b;
-    vector<double> vect_b;
+    double var_b; //initialize input of vector components
+    vector<double> vect_b; //initialize vector with variable values as doubles
     for (double i = 0; i < size; i++)
     {
         cout << "what is the value for element " << i << " in your vector B? " << endl;
-        cin >> var_b;
-        vect_b.push_back(var_b);
+        cin >> var_b; //input for vector b variables
+        vect_b.push_back(var_b); //used to push elements into a vector from the back
     }
     
     cout << endl;
 
     cout << "The answer is: " << endl;
+	//for loop to find the dot product
     for (double i = 0; i < size; i++) {
-        //its only multiplying the last two entries in each input (0.0001*20.945)
+	//dot product that runs for the number inputted for the size
+	//the vector elements in slot 0 are multiplied and added to the existing dot product
+	//this loop contines until i is found to be greater than the size of the matrix
         dot_pro = dot_pro+vect_a[i] * vect_b[i];
     }
     
-
+	//return the value of dot_product
     return dot_pro;
 }
 
 int main()
 {
+	//output the function dot_produuct
    cout << dot_product() << endl;
 	
 	return 0;
