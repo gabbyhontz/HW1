4c390c3fc6638362f426bff3c2481a4544152eaa
gabbyhontz <143756143+gabbyhontz@users.noreply.github.com>
2023-09-26 16:24:17 -0400

    (final)

--- Pro6.cpp
+++ Pro6.cpp
@@ -1,15 +1,13 @@
-﻿// Pro6.cpp : Defines the entry point for the application.
+// Pro6.cpp : Defines the entry point for the application.
 //
 
 #include "Pro6.h"
 
 using namespace std;
 
-//initialize variables
-double grad_cost = 0, y_predict=0, y=0;
-
 //function that solves for the gradient cost using the variable y_predict and y
-double gradient_cost() {
+double gradient_cost(double y_predict, double y) {
+	double grad_cost;
 	grad_cost = 2 * (y_predict - y);
 	return grad_cost; //the value for gradient cost in returned
 }
@@ -17,15 +15,15 @@ double gradient_cost() {
 int main()
 {
 	//a cout statement is made to input a value for y_predict
-	cout << "Input the value for y_predict (using atleast one decimal place): ";
+	double y_predict;
+	cout << "Input the value for y_predict: ";
 	cin >> y_predict;
 
 	//a cout statement is amde to input a value for y
-	cout << "Input the value for y (using atleast one decimal place): ";
+	double y;
+	cout << "Input the value for y: ";
 	cin >> y;
 
 	//the function for gradient cost is called and displayed in the code
-	cout << "Gradient Cost: " << gradient_cost();
-
-	return 0;
+	cout << "Gradient Cost: " << gradient_cost(y_predict, y);
 }
