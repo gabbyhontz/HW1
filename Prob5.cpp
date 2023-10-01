b703d3d53bf626bca0b24adbd668973f2c063388
gabbyhontz <143756143+gabbyhontz@users.noreply.github.com>
2023-09-30 20:46:03 -0400

    FINAL

--- Prob5.cpp
+++ Prob5.cpp
@@ -1,4 +1,4 @@
-﻿// Prob5.cpp : Defines the entry point for the application.
+// Prob5.cpp : Defines the entry point for the application.
 //
 
 #include "Prob5.h"
@@ -19,17 +19,52 @@ double gradient_sigmoid(double sigma_z) {
 	return grad_sig_z;
 }
 
+
+//function for dot product between two vectors
+double dot_product(vector<double> w, vector<double> x, int size) {
+    double dot_pro = 0; //initialize dot product
+
+    //for loop performing the dot product for each vector element 
+    for (double i = 0; i < size; i++) {
+
+        dot_pro = dot_pro + w[i] * x[i];
+    }
+    return dot_pro;
+}
+
 int main()
 {
-	//the value for z is inputted by the user and inputted into the function sigmoid
-	double z;
-	cout << "Type in a value for z (with at least one decimal place): ";
-	cin >> z;
+    int size; //initializing size vector
+    cout << "What is the size of your vectors?" << endl;
+    cin >> size; //taking size of vector from user
 
-	//sigmoid is outputted
-	cout << "Sigma(z) = " << sigmoid(z) << "\n" << endl; //calling the function and dsiplaying the returned value from sigmoid
-	//gradient sigmoid is ouputted where sigmoid is inputted into gradient sigmoid
-	cout << "Gradient Sigma(z) = " << gradient_sigmoid(sigmoid(z)) << "\n" << endl; //calling the function and displaying the returned value from gradient_sigmoid
+    double var_ww; //initialize input of vector components
+    vector<double> vect_ww; //initialize vector with variable values as doubles
+    for (double i = 0; i < size; i++)
+    {
+        cout << "what is the value for element " << i << " in your vector w? " << endl;
+        cin >> var_ww; //input for vector a variables
+        vect_ww.push_back(var_ww); //used to push elements into a vector from the back
+    }
 
-	return 0;
+    double var_xx; //initialize input of vector components
+    vector<double> vect_xx; //initialize vector with variable values as doubles
+    for (double i = 0; i < size; i++)
+    {
+        cout << "what is the value for element " << i << " in your vector x? " << endl;
+        cin >> var_xx; //input for vector a variables
+        vect_xx.push_back(var_xx); //used to push elements into a vector from the back
+    }
+
+    //output the dot product
+    cout << "Dot Product = " << dot_product(vect_ww, vect_xx, size) << "\n" << endl;
+    double z = dot_product(vect_ww, vect_xx, size);
+
+    //sigmoid is outputted
+    cout << "Sigma(z) = " << sigmoid(z) << "\n" << endl; //calling the function and dsiplaying the returned value from sigmoid
+    //gradient sigmoid is ouputted where sigmoid is inputted into gradient sigmoid
+    cout << "Gradient Sigma(z) = " << gradient_sigmoid(sigmoid(z)) << "\n" << endl; //calling the function and displaying the returned value from gradient_sigmoid
+
+
+    return 0;
 }
